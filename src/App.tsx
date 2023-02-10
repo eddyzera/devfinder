532395271d4679c0428b6b25e73cdfb4d5ce2dcc
eddyzera <esilva644@gmail.com>
2023-02-10 15:00:16 -0300

    chore: use stitches and configuration style

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,5 @@
-import { useState } from 'react'
+import React from 'react'
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <h1>hello</h1>
-  )
+export const App: React.FunctionComponent = () => {
+  return <h1>hello</h1>
 }
-
-export default App
---
+++ src/styles/stitches.config.ts
@@ -0,0 +1,34 @@
+import { createStitches } from '@stitches/react'
+
+export const { styled } = createStitches({
+  theme: {
+    colors: {
+      gray900: '#000000',
+      gray800: '#1f1f1f',
+      gray700: '#2e2e2e',
+      gray600: '#313131',
+      gray500: '#969593',
+      gray400: '#a6a6a6',
+      gray300: '#bdbbb7',
+      gray200: '#f1f1f1',
+      gray100: '#ffffff',
+
+      green500: '#45ffbc',
+      olive500: '#e3ffa8',
+    },
+
+    fontSizes: {
+      1: '1.125rem',
+      2: '1.25rem',
+      3: '2rem',
+      4: '3rem',
+      5: '0.625rem',
+      6: '0.75rem',
+      7: '0.875rem',
+      8: '1rem',
+      9: '1.125rem',
+      10: '1.25rem',
+      11: '1.5rem',
+    },
+  },
+})
